b3d1a511bb5b36385f8293ed3b0ed83310fb2d0e
Osabuohien Randalf Ehigiator <107191688+guyrandalf@users.noreply.github.com>
2023-10-30 17:29:20 +0100

    Done

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,56 @@
+// select HTML elements to edit
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const windSpeed = document.querySelector('#wind-speed');
+const windChill = document.querySelector('#feels-like');
+
+
+//API URL with arguments  
+// id for fairbanks alaska is 5861897
+const url = 'https://api.openweathermap.org/data/2.5/weather?id=5861897&appid=d10750740ac29f4c233177823a633962';
+
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    let k = data.main.temp;
+    f = ((k-273.15) * (9/5) + 32).toFixed(0);
+    currentTemp.textContent = f;
+    // currentTemp.innerHTML = `<strong>${data.main.temp.toFixed(0)}</strong>`
+
+    // wind chill
+    // windSpeed.innerHTML = `<strong>${data.wind.speed.toFixed(0)}</strong>`;
+    let w = data.wind.speed.toFixed(0);
+    windSpeed.textContent = w;
+
+
+
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const desc = data.weather[0].description;
+
+    // weather icon
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+
+    // wind chill
+    builtWC(w, f);
+  });
+
+   function builtWC(w, f){
+     //compute the windchill
+     let wc = 35.74 + (0.6215 * f) - (35.75 * Math.pow(w, 0.16)) + (0.4275 * f * Math.pow(w, 0.16));
+
+     //round answer down to nearest whole number
+     wc = Math.floor(wc);
+
+     //if chill is greater than temp, just return the temp
+     wc = (wc > f) ? f : wc;
+
+     //display the windchill
+     if (w > 3 && f < 50)
+        windChill.innerHTML = "Feels like: " + wc + "º";
+     else
+     windChill.innerHTML = "No wind chill";
+    };
