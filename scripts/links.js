f9af34cd59bc8e46e212db7dd695012c650e734f
Randalf Ehigaitor <randalfosabuohien@gmail.com>
2023-11-11 07:59:40 +0100

    testing links now

--- scripts/links.js
+++ scripts/links.js
@@ -2,9 +2,32 @@ const baseURL = "https://guyrandalf.github.io/wdd230/";
 const linksURL = "https://guyrandalf.github.io/wdd230/data/links.json";
 
 async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    console.log(data);
+    try {
+        const response = await fetch(linksURL);
+        const data = await response.json();
+        displayLinks(data.lessons);        
+    } catch (error) {
+        console.error('Error fetching data:', error)
+    }
+}
+
+function displayLinks(lessons) {
+    const list = document.getElementById('linksList');
+  
+    lessons.forEach((lesson) => {
+      lesson.links.forEach((link) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${lesson.lesson}: `;
+        const anchor = document.createElement('a');
+        anchor.href = baseURL + link.url;
+        anchor.textContent = link.title;
+        listItem.appendChild(anchor);
+        list.appendChild(listItem);
+      });
+    });
   }
   
-  getLinks();
+
+  window.onload = function () {
+    getLinks();
+  };
