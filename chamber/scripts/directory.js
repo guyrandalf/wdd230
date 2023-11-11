b3453a00607b8f739baa2ab7278f7fa3d30ea1c4
Randalf Ehigaitor <randalfosabuohien@gmail.com>
2023-11-11 12:44:01 +0100

    Directory done

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,12 +1,5 @@
-// store the URL of the JSON file into a const variable
-const requestURL = 'json/data.json';
+const requestURL = 'json/members.json';
 const cards = document.querySelector('.cards');
-
-// basic fetch() and feed it requiered argument 
-//  the .then() returns a Promise 
-// the seconds .then() is setup for us to work with the converted responce data in javascript object format 
-
-
 
 fetch(requestURL)
 
@@ -14,19 +7,14 @@ fetch(requestURL)
         return response.json();
     })
 
-    .then(function (jsonObject) {
-
-        // store the results of the converted response into an array 
+    .then(function (jsonObject) {        
         const companies = jsonObject['cards'];
-
-        // loop through every record and process each one into its own 'card' (HTML output)in a function named "displayProphets"
         companies.forEach(displayCompanies);
         
     });
 
 
 function displayCompanies(company) {
-    // create elements to add to the document
     let card = document.createElement('section');
     let h3 = document.createElement('h3');
     let portrait = document.createElement('img');
@@ -34,18 +22,13 @@ function displayCompanies(company) {
     let p2 = document.createElement('p');
     let p3 = document.createElement('p');
     let p4 = document.createElement('p');
-    
-    
-
-    // change the text content property of the h2 element to contain the prophets full name
+  
     h3.textContent = `${company.name}`;
 
-    // build the image attributes bu using the set attribute method for the src, allt, and loading attribute values
     portrait.setAttribute('src', company.imageurl);
     portrait.setAttribute('alt', `Company image of ${company.name}`);
     portrait.setAttribute('loading', 'lazy');
 
-    // chamge the text content of property of the h1 element to contain the prophet info
     p1.textContent = `Address: ${company.address}`;
     p1.classList.add("address");
     p2.textContent = `Phone: ${company.phone}`;
@@ -53,9 +36,6 @@ function displayCompanies(company) {
     p3.textContent = `Website: ${company.website}`;
     p4.textContent = `Membership: ${company.membership}`;
 
-
-
-    // append the seciton with the h2 element 
     card.appendChild(h3);
     card.appendChild(portrait);
     card.appendChild(p1);
@@ -63,25 +43,19 @@ function displayCompanies(company) {
     card.appendChild(p3);
     card.appendChild(p4);
     
-
-    // append the existing HTML div with the cards class with the section
     document.querySelector('section.cards').appendChild(card);
 }
 
-// display cards as grid and list
 const gridbutton = document.querySelector("#grid-button");
 const listbutton = document.querySelector("#list-button");
 const display = document.querySelector(".cards");
 
-// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
-
 gridbutton.addEventListener("click", () => {
-	// example using arrow function
 	display.classList.add("cardsgrid");
 	display.classList.remove("cardslist");
 });
 
-listbutton.addEventListener("click", showList); // example using defined function
+listbutton.addEventListener("click", showList);
 
 function showList() {
 	display.classList.add("cardslist");
