ceed36c4df49b0a230d623b05302e613098dfa6f
Randalf Ehigaitor <randalfosabuohien@gmail.com>
2023-11-17 16:46:22 +0100

    WeatherAPI

---
+++ scripts/weather.js
@@ -0,0 +1,40 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const weatherDesc = document.querySelector("#desc");
+
+let lat = 7.385555104478326
+let lon = 3.9476320227986013
+
+let latFixed = lat.toFixed(2)
+let lonFixed = lon.toFixed(2)
+
+const url =
+  `https://api.openweathermap.org/data/2.5/weather?lat=${latFixed}&lon=${lonFixed}&appid=1aa127830d80bc4014569d5f214be441`
+async function apiFetch(){
+    try {
+        const response = await fetch(url)
+        if (response.ok) {
+            const data = await response.json()
+            console.log(data)
+            displayResults(data)
+        } else {
+            throw Error(await response.text())
+        }
+    } catch (error) {
+        
+    }
+}
+
+function displayResults(data) {
+    farenheitValue = data.main.temp
+    // celciusValue = (5 * (farenheitValue - 32) /9).toFixed(0)
+    currentTemp.innerHTML = `${farenheitValue}&deg;F`
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+    let desc = data.weather[0].description
+    weatherIcon.setAttribute('src', iconsrc)
+    weatherIcon.setAttribute('alt', desc)
+    weatherDesc.textContent = `${desc.toUpperCase()}`
+}
+
+apiFetch()
+
