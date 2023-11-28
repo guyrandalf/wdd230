aa768f21303b335224f22041884c3d3cbb2a0f85
Randalf Ehigaitor <randalfosabuohien@gmail.com>
2023-11-28 07:47:21 +0100

    WAVE Error Fixed

--- scripts/app.js
+++ scripts/app.js
@@ -19,34 +19,12 @@ document.getElementById(
   "lastModified"
 ).textContent = `Last Updated ${currentdate}`;
 
-const kp1 = document.querySelector("#password");
-const kp2 = document.querySelector("#password2");
-const message = document.querySelector("#formmessage");
+let visitCountElement = document.getElementById("visitCount");
 
-kp2.addEventListener("focusout", checkSame);
-
-// This should be refactored.
-function checkSame() {
-  if (kp1.value !== kp2.value) {
-    message.textContent = "❗Passwords DO NOT MATCH!";
-    message.style.visibility = "show";
-    kp2.style.backgroundColor = "#fff0f3";
-    kp2.value = "";
-    kp1.focus();
-  } else {
-    message.style.display = "none";
-    kp2.style.backgroundColor = "#fff";
-    kp2.style.color = "#000";
-  }
-}
-
-const rangevalue = document.getElementById("rangevalue");
-const range = document.getElementById("rating");
-
-// RANGE event listener
-range.addEventListener('change', displayRatingValue);
-range.addEventListener('input', displayRatingValue);
-
-function displayRatingValue() {
-    rangevalue.innerHTML = range.value;
+if (localStorage.getItem("pageVisits")) {
+  let count = parseInt(localStorage.getItem("pageVisits"));
+  visitCountElement.textContent = count;
+} else {
+  localStorage.setItem("pageVisits", 1);
+  visitCountElement.textContent = 1;  
 }
--- scripts/weather.js
+++ scripts/weather.js
@@ -2,36 +2,44 @@ const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
 const weatherDesc = document.querySelector("#desc");
 
+const captionDesc = document.querySelector("figcaption");
+const city = document.querySelector(".location");
+const country = document.querySelector("#country");
+
 let lat = 7.385555104478326;
 let lon = 3.9476320227986013;
 
 let latFixed = lat.toFixed(2);
 let lonFixed = lon.toFixed(2);
 
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latFixed}&lon=${lonFixed}&units=imperial&appid=1aa127830d80bc4014569d5f214be441`;
+const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latFixed}&lon=${lonFixed}&units=metric&appid=1aa127830d80bc4014569d5f214be441`;
 
 async function apiFetch() {
   try {
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
-      // console.log(data);
       displayResults(data);
     } else {
       throw Error(await response.text());
     }
-  } catch (error) {}
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
 
 function displayResults(data) {
-  farenheitValue = data.main.temp;
-  celciusValue = ((5 * (farenheitValue - 32)) / 9).toFixed(0);
-  currentTemp.innerHTML = `${celciusValue}&deg;C`;
-  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-  let desc = data.weather[0].description;
+  const currentTemperature = data.list[0].main.temp;
+  const currentDescription = data.list[0].weather[0].description;
+  currentTemp.innerHTML = `${currentTemperature}&deg;C`;
+  captionDesc.textContent = currentDescription.toUpperCase();
+  city.innerHTML = data.city.name;
+  country.innerHTML = data.city.country;
+
+  const iconsrc = `https://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`;
+  let desc = data.list[0].weather[0].description;
+
   weatherIcon.setAttribute("src", iconsrc);
   weatherIcon.setAttribute("alt", desc);
-  weatherDesc.textContent = `${desc.toUpperCase()}`;
 }
-
 apiFetch();
