34c61d89d867565d959ed81a016508013ecdd1ac
Randalf Ehigaitor <randalfosabuohien@gmail.com>
2023-11-28 18:35:32 +0100

    Discover Page

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,48 +1,53 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Lazy loading images on scroll
-    let lazyLoadImages = document.querySelectorAll('.lazy');
+document.addEventListener("DOMContentLoaded", function () {
+  // Lazy loading images on scroll
+  let lazyLoadImages = document.querySelectorAll('img[loading="lazy"]');
 
-    let lazyLoad = function() {
-        lazyLoadImages.forEach(function(img) {
-            if (img.getBoundingClientRect().top < window.innerHeight * 2 && img.getBoundingClientRect().bottom >= 0 && getComputedStyle(img).display !== 'none') {
-                img.src = img.getAttribute('data-src');
-                img.classList.remove('lazy');
-                
-                lazyLoadImages = Array.from(lazyLoadImages).filter(function(image) {
-                    return image !== img;
-                });
+  let lazyLoad = function () {
+    lazyLoadImages.forEach(function (img) {
+      if (
+        img.getBoundingClientRect().top < window.innerHeight * 2 &&
+        img.getBoundingClientRect().bottom >= 0 &&
+        getComputedStyle(img).display !== "none"
+      ) {
+        img.src = img.getAttribute("data-src");
+        img.classList.remove("lazy");
 
-                if (lazyLoadImages.length === 0) {
-                    document.removeEventListener('scroll', lazyLoad);
-                    window.removeEventListener('resize', lazyLoad);
-                    window.removeEventListener('orientationchange', lazyLoad);
-                }
-            }
+        lazyLoadImages = Array.from(lazyLoadImages).filter(function (image) {
+          return image !== img;
         });
-    };
 
-    document.addEventListener('scroll', lazyLoad);
-    window.addEventListener('resize', lazyLoad);
-    window.addEventListener('orientationchange', lazyLoad);
-    
-    const visitMessageElement = document.getElementById('visitMessage');
-    const lastVisit = localStorage.getItem('lastVisit');
-
-    if (!lastVisit) {        
-        visitMessageElement.textContent = 'Welcome! Let us know if you have any questions.';
-    } else {
-        const currentTime = new Date().getTime();
-        const timeDiff = currentTime - lastVisit;
-        const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-
-        if (daysDiff < 1) {
-            visitMessageElement.textContent = 'Back so soon! Awesome!';
-        } else if (daysDiff === 1) {
-            visitMessageElement.textContent = 'You last visited 1 day ago.';
-        } else {
-            visitMessageElement.textContent = `You last visited ${daysDiff} days ago.`;
+        if (lazyLoadImages.length === 0) {
+          document.removeEventListener("scroll", lazyLoad);
+          window.removeEventListener("resize", lazyLoad);
+          window.removeEventListener("orientationchange", lazyLoad);
         }
+      }
+    });
+  };
+
+  document.addEventListener("scroll", lazyLoad);
+  window.addEventListener("resize", lazyLoad);
+  window.addEventListener("orientationchange", lazyLoad);
+
+  const visitMessageElement = document.getElementById("visitMessage");
+  const lastVisit = localStorage.getItem("lastVisit");
+
+  if (!lastVisit) {
+    visitMessageElement.textContent =
+      "Welcome! Let us know if you have any questions.";
+  } else {
+    const currentTime = new Date().getTime();
+    const timeDiff = currentTime - lastVisit;
+    const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+    if (daysDiff < 1) {
+      visitMessageElement.textContent = "Back so soon! Awesome!";
+    } else if (daysDiff === 1) {
+      visitMessageElement.textContent = "You last visited 1 day ago.";
+    } else {
+      visitMessageElement.textContent = `You last visited ${daysDiff} days ago.`;
     }
-    
-    localStorage.setItem('lastVisit', new Date().getTime());
+  }
+
+  localStorage.setItem("lastVisit", new Date().getTime());
 });
